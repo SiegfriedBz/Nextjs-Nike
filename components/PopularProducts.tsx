c28c2be7e5d796da16afefb4a545985b4b211c78
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-25 15:45:26 +0100

    add PopularProducts

--- components/PopularProducts.tsx
+++ components/PopularProducts.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import PopularProductCard from './PopularProductCard'
+import { PRODUCTS } from '@/constants'
 
 const PopularProducts = () => {
-  return <div>PopularProducts</div>
+  return (
+    <div className='flex flex-col gap-16'>
+      {/* text */}
+      <div className='flex flex-col justify-start gap-8'>
+        <h2 className='text-5xl font-palanquin font-bold capitalize'>
+          Our <span className='text-coral-red'>Popular</span> products
+        </h2>
+        <p className='font-montserrat text-slate-gray sm:w-2/3'>
+          Experiencde top notch quality and style with our sought after
+          selection. Discover a world of comfort, design, and value.
+        </p>
+      </div>
+
+      {/* products */}
+      <div className='flex flex-wrap items-center justify-center gap-8 sm:justify-start sm:gap-4'>
+        {PRODUCTS.map((product, index) => {
+          /* product card */
+          return <PopularProductCard key={index} product={product} />
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default PopularProducts
